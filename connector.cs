fb7a1d80f2adbdc90898204ba6248cc5a2e9f4ae
YA <YA-androidapp@users.noreply.github.com>
2022-02-18 17:54:30 +0900

    Update connector.cs

--- connector.cs
+++ connector.cs
@@ -2,14 +2,26 @@ public class Script : ScriptBase
 {
     public override async Task<HttpResponseMessage> ExecuteAsync()
     {
-        
-        XmlDocument doc = new XmlDocument();
-        var contentAsJson = JObject.Parse(await Context.Request.Content.ReadAsStringAsync().ConfigureAwait(false));
-        doc.LoadXml((string)contentAsJson["xml"]);
-        var jsonStr = JsonConvert.SerializeXmlNode(doc);
+        HttpResponseMessage response;
 
-        HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
-        response.Content = CreateJsonContent(jsonStr);
+        try
+        {
+            XmlDocument doc = new XmlDocument();
+            var contentAsJson = JObject.Parse(await Context.Request.Content.ReadAsStringAsync().ConfigureAwait(false));
+            string xmlStr = (string)contentAsJson["xml"];
+            if(!String.IsNullOrEmpty(xmlStr)){
+                doc.LoadXml((string)contentAsJson["xml"]);
+                var jsonStr = JsonConvert.SerializeXmlNode(doc);
+
+                response = new HttpResponseMessage(HttpStatusCode.OK);
+                response.Content = CreateJsonContent(jsonStr);
+                return response;
+            }
+        } catch (Exception e)
+        {
+        }
+
+        response = new HttpResponseMessage(HttpStatusCode.BadRequest);
         return response;
     }
 }
